f8c6aeae9c479b92d64d7fd47351fc09f45bb129
Arka Sadhu <ark.sadhu2904@gmail.com>
2018-03-28 02:18:51 +0530

    boilerplate code for opt written

--- code/main.py
+++ code/main.py
@@ -1,5 +1,25 @@
 import numpy as np
 from data_loader import synthetic_data_gen
 
+
+def solve_L(Y, alpha, beta):
+    L = np.zeros(Y.shape[0], Y.shape[0])
+    return L
+
+
+def gl_sig_model(inp_signal, max_iter, alpha, beta):
+    """
+    Returns Output Signal Y, Graph Laplacian L
+    """
+    y = inp_signal
+    ldim = inp_signal.shape[1]
+    for _ in range(max_iter):
+        # Update L
+        L = solve_L(Y, alpha, beta)
+        # Update Y
+        Y = (np.eye(ldim) + alpha * L)
+    return L, Y
+
+
 if __name__ == "__main__":
     syn = synthetic_data_gen()
